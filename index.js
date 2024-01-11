27203836ab9ae24f95066b75632f7c0a22d61547
nescpro <salazarnestor1996@gmail.com>
2024-01-11 16:43:09 -0500

    Create the connection to the database

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import  express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config()
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE_URL);
+}
 
 const app = express();
 
