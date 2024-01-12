0d03c197bf9327fa8e188d17848798cfbcd12758
nescpro <salazarnestor1996@gmail.com>
2024-01-11 20:21:05 -0500

    Create signup api route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import  express from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 import { router as usersRouter } from "./routes/users.route";
+import { router as authRouter } from "./routes/auth.route";
 
 config()
 
@@ -12,8 +13,12 @@ async function main() {
 }
 
 const app = express();
+app.disable('x-powered-by')
 
-app.use('/users', usersRouter)
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
