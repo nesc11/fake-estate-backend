427e298e559b282d8381d1244174c659b8ce173b
nescpro <salazarnestor1996@gmail.com>
2024-01-13 15:53:30 -0500

    Add the google authenticatication api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,3 +30,27 @@ export const signin = async (req, res, next) => {
         next(error)
     }
 }
+
+export const google = async (req, res, next) => {
+    const { username, email, photo } = req.body
+    console.log('here')
+
+    try {
+        const user = await User.findOne({ email })
+        if (user) {
+            const token = createJWT(user)
+            const { password: pass, ...rest } = user._doc
+            res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+        } else {
+            const generatedPassword = Math.random().toString(36).slice(-8)
+            const generatedUsername = username.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4)
+            const newUser = new User({ username: generatedUsername, email, password: hashPassword(generatedPassword), avatar: photo })
+            await newUser.save()
+            const token = createJWT(newUser)
+            const { password: pass, ...rest } = newUser._doc
+            res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+        }
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
+import { signup, signin, google } from "../controllers/auth.controller.js";
 
 export const router = Router()
 
 router.post('/sign-up', signup)
 router.post('/sign-in', signin)
+router.post('/google', google)
